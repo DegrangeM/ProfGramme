9a68ac6eb05be0db2b9d4ddafc646ed50a85460a
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2020-08-20 03:26:33 +0200

    Compatibilité Progressive Web App

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+	  caches.open('v1').then((cache) => {
+		return cache.addAll([
+			'./index.html',
+			'./main.css',
+			'./jqueryui/jquery-ui.css',
+			'./jqueryui/external/jquery/jquery.js',
+			'./jqueryui/jquery-ui.js',
+			'./example.js',
+			'./source.js',
+			'./ui.js',
+			'./main.js',
+			'./logo.svg',
+		]);
+	  })
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		fetch(event.request).catch(function() {
+			return caches.match(event.request);
+		})
+	);
+});
+
+/*
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+		return response || fetch(event.request);
+		})
+	);
+});
+*/
