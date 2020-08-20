e1b456033e44296f72f5ee60156de8f11fe6c2dd
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2020-08-20 03:51:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
 	event.waitUntil(
-	  caches.open('v1').then((cache) => {
+	  caches.open('ProfGramme').then((cache) => {
 		return cache.addAll([
 			'./',
 			'./index.html',
